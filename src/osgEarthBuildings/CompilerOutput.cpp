b142053ee71a9377f4b8553875be2c10deb6e8cd
gwaldron <gwaldron@gmail.com>
2016-01-15 14:31:09 -0500

    Add instancing for roof (and other) models.

--- src/osgEarthBuildings/CompilerOutput.cpp
+++ src/osgEarthBuildings/CompilerOutput.cpp
@@ -19,6 +19,8 @@
 #include "CompilerOutput"
 #include <osg/MatrixTransform>
 #include <osgEarth/ShaderGenerator>
+#include <osgEarth/DrawInstanced>
+#include <osgEarth/Registry>
 
 using namespace osgEarth;
 using namespace osgEarth::Buildings;
@@ -41,42 +43,57 @@ CompilerOutput::addInstance(ModelResource* model, const osg::Matrix& matrix)
 osg::Node*
 CompilerOutput::createSceneGraph(Session* session) const
 {
+    // install the master matrix for this graph:
     osg::ref_ptr<osg::MatrixTransform> root = new osg::MatrixTransform( getLocalToWorld() );
     
+    // add the main geode.
     if ( getMainGeode()->getNumDrawables() > 0 )
         root->addChild( getMainGeode() );
     
+    // add the detail geode with a closer LOD range.
     if ( getDetailGeode()->getNumDrawables() > 0 )
         root->addChild( getDetailGeode() ); // TODO: add LOD here
 
+    // debug information.
     if ( getDebugGroup()->getNumChildren() > 0 )
         root->addChild( getDebugGroup() );
     
+    // install the model instances, creating one instance group for each model.
     if ( session && session->getResourceCache() )
     {
-        osg::Group* models = new osg::Group();
-        models->setDataVariance( models->DYNAMIC ); // stop the optimizer
-        ShaderGenerator::setIgnoreHint( models, true ); // stop the shader generator
-        root->addChild( models );
+        osg::Group* instances = new osg::Group();
+        instances->setDataVariance( instances->DYNAMIC ); // block the optimizer
+        ShaderGenerator::setIgnoreHint( instances, true ); // block the shader generator
 
         for(ModelPlacementMap::const_iterator i = _instances.begin(); i != _instances.end(); ++i)
         {
             osg::ref_ptr<osg::Node> modelNode;
-            if ( session->getResourceCache()->getOrCreateInstanceNode(i->first.get(), modelNode) )
+            if ( session->getResourceCache()->cloneOrCreateInstanceNode(i->first.get(), modelNode) )
             {
-                osg::Group* group = new osg::Group();
-                models->setDataVariance( models->DYNAMIC ); // stop the optimizer
-                ShaderGenerator::setIgnoreHint( models, true ); // stop the shader generator
-                root->addChild( group );
+                Registry::instance()->shaderGenerator().run( modelNode.get(), session->getStateSetCache() );
 
+                osg::Group* group = new osg::Group();
+                instances->addChild( group );
+
+                // Build a normal scene graph based on MatrixTransforms, and then convert it 
+                // over to use instancing if it's available.
                 for(MatrixVector::const_iterator m = i->second.begin(); m != i->second.end(); ++m)
                 {
                     osg::MatrixTransform* modelxform = new osg::MatrixTransform( *m );
                     modelxform->addChild( modelNode.get() );
                     group->addChild( modelxform );
                 }
+
+                // fails gracefully if instancing is not available:
+                DrawInstanced::convertGraphToUseDrawInstanced( group );
             }
         }
+        
+        // fails gracefully if instancing is not available:
+        DrawInstanced::install( instances->getOrCreateStateSet() );
+
+        // finally add all the instance groups.
+        root->addChild( instances );
     }
 
     return root.release();
